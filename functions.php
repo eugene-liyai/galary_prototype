24493a617f0c090ae67219837c8317dbddca17a3
Andela-eugene <eugene.liyai@andela.com>
2018-09-20 13:06:38 +0300

    [Feat: #] add function exists check

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/* Function file */
+
+    if (!function_exists('gallery_setup')):
+        function gallery_setup() {
+            // WP handle title tags
+            add_theme_support('title_tag');
+        }
+    endif;
+?>
+
