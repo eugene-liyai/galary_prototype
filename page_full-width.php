b124d67eae3c0c05442b5ea3865b5fc1191349d1
Andela-eugene <eugene.liyai@andela.com>
2018-09-25 15:15:44 +0300

    [Feat: #] add content within loop

--- page_full-width.php
+++ page_full-width.php
@@ -9,16 +9,18 @@
             <h2 class="text-center text-uppercase text-white"><?php echo get_the_title(); ?></h2>
             <hr class="star-light mb-5">
             <div class="row">
-                <?php
-                    if (have_posts()):
-                        while(have_posts()):
-                            the_post();
-                            the_content();
-                        endwhile;
-                    endif;
-                ?>
                 <div class="col-lg-12 ml-auto">
-                    <p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
+                    <p class="lead">
+                    <?php
+                        if (have_posts()):
+                            while(have_posts()):
+
+                                the_post();
+                                the_content();
+                            endwhile;
+                        endif;
+                    ?>
+                    </p>
                 </div>
             </div>
         </div>
